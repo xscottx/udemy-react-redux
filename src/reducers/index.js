e796892d9b86d299076d30edff437414166e60c0
Hoang Vo <hoangvo@gmail.com>
2017-07-04 12:27:51 -0500

    finished section 4

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,10 @@
 // a reducer is a function that returns a piece of the state
 import { combineReducers } from 'redux';
-import BooksReducer  from './reducer_books';
-
+import BooksReducer from './reducer_books';
+import ActiveBook from './reducer_active_book';
 const rootReducer = combineReducers({
-  books: BooksReducer
+  books: BooksReducer,
+  activeBook: ActiveBook
 });
 
 export default rootReducer;
