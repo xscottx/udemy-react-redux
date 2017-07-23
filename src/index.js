1116a0e419cde1b1da69a1938dc04a27ad99869a
Hoang Vo <hoangvo@gmail.com>
2017-07-23 11:19:03 -0500

    feature: added routes for pages

--- src/index.js
+++ src/index.js
@@ -2,14 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route } from 'react-router-dom';
+import PostsIndex from './components/posts_index';
 
-import App from './components/app';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <BrowserRouter>
+      <div>
+        <Route path="/" component={PostsIndex} />
+        {/* <Route path="/posts/:id" compoennt={PostsShow} /> */}
+        {/* <Route path="/posts/new" compoennt={PostsNew} /> */}
+      </div>
+    </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
