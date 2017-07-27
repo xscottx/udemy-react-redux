b08917369c3c0ec2ec6f14e7dd1fd2e82abcac3b
Hoang Vo <hoangvo@gmail.com>
2017-07-27 07:38:37 -0500

    chore: initial form plumbinb

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,13 +1,45 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions';
+import _ from 'lodash';
+import { Link } from 'react-router-dom';
 
 class PostsIndex extends Component {
+  componentDidMount() {
+    // lifecycle method that will be immediately called after rendered
+    // componentWillMount is lifecycle method that will call before rendered
+    this.props.fetchPosts();
+  }
+
+  renderPosts() {
+      return _.map(this.props.posts, post => {
+        return (
+          <li className="list-group-item" key={post.id}>
+            {post.title}
+          </li>
+        )
+      })
+  }
+
   render() {
     return (
       <div>
-        Posts Index
+        <div className="text-xs-right">
+          <Link className="btn btn-primary" to="/posts/new">
+            Add a Post
+          </Link>
+        </div>
+        <h3>Posts</h3>
+        <ul className="list-group">
+          {this.renderPosts()}
+        </ul>
       </div>
     );
   }
 }
 
-export default PostsIndex;
+function matchStateToProps(state) {
+  return { posts: state.posts };
+}
+
+export default connect(matchStateToProps, { fetchPosts })(PostsIndex);
--- src/index.js
+++ src/index.js
@@ -2,20 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import PostsIndex from './components/posts_index';
+import PostsNew from './components/posts_new';
 
+import promise from 'redux-promise';
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
+// put most specific route at top on Switch
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div>
-        <Route path="/" component={PostsIndex} />
-        {/* <Route path="/posts/:id" compoennt={PostsShow} /> */}
-        {/* <Route path="/posts/new" compoennt={PostsNew} /> */}
+        <Switch>
+          <Route path="/posts/new" component={PostsNew} />
+          <Route path="/" component={PostsIndex} />
+          {/* <Route path="/posts/:id" component={PostsShow} /> */}
+        </Switch>
       </div>
     </BrowserRouter>
   </Provider>
